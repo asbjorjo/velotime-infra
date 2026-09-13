0f7f7f970d9bbd4c8ba56b34d6bf7967ea28b706
Asbjoern L. Johansen <asbjorjo@gmail.com>
2026-09-13 17:31:09 +0000

    feat: Use S3 backend for state

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    key                         = "velotime-infra/terraform.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+  }
+
   required_providers {
     upcloud = {
       source  = "UpCloudLtd/upcloud"
