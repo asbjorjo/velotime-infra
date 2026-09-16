85f0e406c8bf32eb4d9eb77bc6a8163b79af7f0a
Asbjoern L. Johansen <asbjj@equinor.com>
2026-09-16 15:18:12 +0000

    Do not create kubefonfig

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ module "cluster" {
   source = "./cluster"
 
   basename         = var.basename
-  store_kubeconfig = true
+  store_kubeconfig = false
   zone             = var.zone
   plan             = var.cluster_plan
   node_plan        = var.cluster_node_plan
