9be1130936642f458d047598f6b6148c753b8a7f
Asbjoern L. Johansen <asbjj@equinor.com>
2026-09-17 11:29:38 +0000

    Add termination protection to production

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -85,3 +85,9 @@ variable "database_password" {
   type        = string
   sensitive   = true
 }
+
+variable "termination_protection" {
+  description = "Protect resources from deletion and shutdown"
+  type        = bool
+  default     = false
+}
