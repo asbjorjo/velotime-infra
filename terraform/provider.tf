235739e4eea02a9197415b512329d24194fc3f04
Asbjoern L. Johansen <asbjorjo@gmail.com>
2026-09-23 20:37:41 +0000

    Grant database privileges to app users

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,6 +19,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.27"
+    }
   }
 }
 
